af99cddb089edd8b1b279b5d964d53fe4dc9344e
Ximu-Luya <794876939@qq.com>
2023-01-11 15:19:59 +0800

    常规页面的半页banner完成

--- web/src/components/PageBanner.vue
+++ web/src/components/PageBanner.vue
@@ -1,10 +1,15 @@
 <template>
   <section class="banner">
-    <div class="banner-img">
-      <img class="banner-overlay" src="@/assets/img/banner-overlay.png" />
-    </div>
+    <div class="banner-img"></div>
     <div class="banner-content">
-      
+      <div class="title-container">
+        <div class="page-title white-text">BLOGS</div>
+        <div class="page-subtitle">
+          <span class="left line"></span>
+          <div class="subtitle-text white-text">记我所想</div>
+          <span class="right line"></span>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -15,7 +20,7 @@
 <style lang="scss" scoped>
 // 封面
 .banner {
-  height: 100vh;
+  height: 460px;
   width: 100%;
   position: relative;
   overflow: hidden;
@@ -41,11 +46,50 @@
 
   // 封面内容
   .banner-content {
+    height: 100%;
     max-width: 1280px;
     margin: 0 auto;
     display: flex;
+    justify-content: center;
+    align-items: center;
     padding: 0 40px;
     
+    .title-container {
+      display: flex;
+      flex-direction: column;
+      text-align: center;
+
+      .page-title {
+        font-family: Oswald;
+        font-size: 44px;
+      }
+      .page-subtitle {
+        font-size: 16px;
+
+        .line {
+          display: inline-block;
+          height: 1px;
+          margin: 0 auto;
+          position: absolute;
+          top: 50%;
+          width: 100px;
+          overflow: hidden;
+          border-bottom-style: solid;
+          width: 100%;
+          max-width: 100%;
+          border-bottom-width: 2px;
+          border-bottom-color: #379cf4;
+          &.left {
+            right: 100%;
+            margin-right: 20px;
+          }
+          &.right {
+            left: 100%;
+            margin-left: 20px;
+          }
+        }
+      }
+    }
   }
 }
 </style>
