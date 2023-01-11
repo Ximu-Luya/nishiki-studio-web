4781ef61a6f143cc7ca4affbb2a736b1586b0093
Ximu-Luya <794876939@qq.com>
2023-01-11 15:19:58 +0800

    完成页面Header显隐逻辑

--- web/src/components/Header.vue
+++ web/src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header>
+  <header class="top-header" :class="{'fixed-header': showFixedHeader}">
     <div class="logo-bar">
       <img src="@/assets/img/logo-bar.png" />
     </div>
@@ -11,38 +11,62 @@
   </header>
 </template>
 
-<script>
-import { RouterLink } from 'vue-router';
+<script setup>
+import { RouterLink } from 'vue-router'
+import { ref, onMounted } from 'vue'
+
+const showFixedHeader = ref(false)
+
+onMounted(() => {
+  window.addEventListener('scroll', () => {
+    const scrollTop = window.pageYOffset
+    console.log(scrollTop);
+    if(scrollTop > 50) showFixedHeader.value = true
+    else showFixedHeader.value = false
+  })
+})
 </script>
 
 <style lang="scss" scoped>
 header {
   padding: 0 40px;
-  position: fixed;
-  top: 0; 
-  height: 80px;
   width: 100%;
   display: flex;
   justify-content: space-between;
   z-index: 10;
+}
 
-  // logo区
-  .logo-bar {
-    padding: 10px 0;
-    img {
-      height: 100%;
-    }
+.top-header {
+  position: absolute;
+  top: 0; 
+  height: 80px;
+}
+
+.fixed-header {
+  position: fixed;
+  background-color: orange;
+  top: 0; 
+  height: 60px;
+  transition: background-color 0.5s;
+}
+
+// logo区
+.logo-bar {
+  z-index: 11;
+  padding: 10px 0;
+  img {
+    height: 100%;
   }
+}
 
-  .nav-bar {
-    display: flex;
-    align-items: center;
+.nav-bar {
+  display: flex;
+  align-items: center;
 
-    .nav-item {
-      margin: 0 10px;
-      padding: 20px;
-      font-weight: bold;
-    }
+  .nav-item {
+    margin: 0 10px;
+    padding: 10px 20px;
+    font-weight: bold;
   }
 }
 </style>
