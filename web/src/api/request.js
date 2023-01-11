1b362d9e5153d41180259dce2280da29d016076e
Ximu-Luya <794876939@qq.com>
2023-01-11 20:35:04 +0800

    请求封装重构，响应不同错误，返回axios实例

--- web/src/api/request.js
+++ web/src/api/request.js
@@ -1,16 +1,15 @@
 import axios from 'axios'
-import Router from '../router/index'
 import { ElMessage } from 'element-plus'
 
+const instance = axios.create({
+  baseURL: 'http://localhost:8087/api'
+})
+
 // 定义axios请求拦截器
-axios.interceptors.request.use(
+instance.interceptors.request.use(
   config => {
     // 在发送数据之前做些什么
-    if (config.method !== 'get') {
-      // 在headers中新增authorization属性来携带token
-      config.headers.authorization = `Bearer ${localStorage.getItem('token')}`
-    }
-    console.log('请求url为：' + config.url, config)
+    // console.log('请求url为：' + config.url, config)
     return config
   },
   err => {
@@ -21,48 +20,38 @@ axios.interceptors.request.use(
 )
 
 // 定义axios响应拦截器
-axios.interceptors.response.use(
+instance.interceptors.response.use(
   response => {
     // 在收到数据之前做些什么
     const { data, config } = response
 
     // 控制台输出接收到的响应全部数据
-    console.log('响应url为：' + config.url, response)
+    // console.log('响应url为：' + config.url, response)
     // 若返回的事件代码不为200（即不为数据响应而是状态响应）为用户提示后端传来的信息
     switch (data.code) {
-      case 200:
-        break
-      case 201:
-      case 202:
-      case 203:
+      case 0:
         ElMessage.success(data.message)
         break
-      case 204:
-        ElMessage.warning(data.message)
-        break
       default:
-        ElMessage.error('响应数据不是规范的数据')
+        ElMessage.error(data.message)
     }
-    // 替换响应数据，将具体数据替换到响应的data中
+    // 替换响应数据，将具体数据替换到响应的data中;
     return data.data
   },
   err => {
     // 对响应错误做些什么
 
     // 控制台输出错误信息
-    console.log(err)
-    // 若错误响应不为空即后台已响应并按规范返回了错误信息
-    if (err.response) ElMessage.error(err.response.data)
-    // 若错误响应为空即后端未进行响应，识别为网络错误
-    else ElMessage.error('网络连接错误')
+    // console.log(err)
 
-    if (err.response.status === 401) {
-      console.log(Vue.prototype)
-      Router.push('/login').catch()
-      console.log(2)
+    if(err.code === "ERR_NETWORK") {
+      ElMessage.error('ERR_NETWORK 服务器连接失败喵QAQ')
+    }
+    if (err.response?.status === 404) {
+      ElMessage.error('404 链接失效喵QAQ')
     }
     return Promise.reject(err)
   }
 )
 
-export default axios
+export default instance
