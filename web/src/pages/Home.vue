9c9bce8098728733f85eb2d2ebc113ee36da5156
Ximu-Luya <794876939@qq.com>
2023-01-11 15:19:54 +0800

    新增主页与路由

---
+++ web/src/pages/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home-page">
+    <section class="banner">
+      <img src="@/assets/banner.png" class="banner-img" />
+      <img src="@/assets/banner-overlay.png" class="banner-overlay" />
+      <div class="intro"></div>
+    </section>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.home-page {
+  display: flex;
+}
+
+// 封面
+.banner {
+  height: 100vh;
+  width: 100%;
+  position: relative;
+
+  // 封面底图
+  .banner-img {
+    height: 100%;
+    width: 100%;
+  }
+
+  // 封面遮罩图
+  .banner-overlay {
+    position: absolute;
+    bottom: 0; left: 0;
+    width: 100%;
+  }
+}
+</style>
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,23 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import PublicPage from '../pages/PublicPage.vue'
+import Home from '../pages/Home.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      redirect: '/home',
-      component: PublicPage,
-      children: [
-        {
-          path: '/home',
-          component: () => import("../components/PublicHome.vue")
-        },
-        {
-          path: '/blog',
-        }
-      ]
+      component: Home,
     },
+    {
+      path: "/blog",
+      component: () => import('../pages/PublicPage.vue')
+    }
   ]
 })
 
